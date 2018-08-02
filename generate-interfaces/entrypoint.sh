84e1ba0fb0ee8253cb91b1b3ec02407bf5fe824c
Dan Imbrogno <dan@3vl.ca>
2018-08-02 16:33:15 -0400

    Initial Commit

---
+++ generate-interfaces/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Wait until an introspection query would work
+# Then run the interface generator
+URL="$INPUT_SERVER_ENDPOINT"
+QUERY="?query=query%20IntrospectionTypeQuery%20%7B__schema%20%7Btypes%20%7Bname%7D%7D%7D"
+echo "Checking $URL"
+wait-on "http-get://$URL$QUERY" &&\
+echo "Generating schema.json" &&\
+apollo-codegen introspect-schema http://"$URL$QUERY" --output schema.json &&\
+echo "Outputing server typings" &&\
+gql-gen --schema schema.json --template graphql-codegen-typescript-template --out $OUTPUT_SERVER_TYPINGS --skip-documents &&\
+echo "Outputting client typings" &&\
+apollo-codegen generate $INPUT_CLIENT_GRAPHQL_FILES --schema schema.json --target typescript --output $OUTPUT_CLIENT_TYPINGS
